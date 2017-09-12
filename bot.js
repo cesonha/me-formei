7fd30eeb1912bcb3cc2204ccfec13566bbbd58f5
Cesar <cecerhcp0@hotmail.com>
2017-09-11 21:46:38 -0300

    added progress bar

--- bot.js
+++ bot.js
@@ -14,9 +14,24 @@ var getRandomMsg = function(msgs) {
 	return msgs[Math.floor(Math.random() * msgs.length)];
 }
 
+var getPercentageBar = function(daysLeft) {
+	var perc = 100 - (100 * (daysLeft / 365))
+	var text = "Quanto já foi do último ano:\n"
+	for (i = 0; i < 20; i++) {
+		if (perc > (i+1) * 5) {
+    		text += "█"
+    	}
+    	else {
+    		text += "▒"
+    	}
+	}
+	text += " " + perc.toFixed(2) + "%"
+}
+
 var motivationalMsgs = ["Vai Cesar, pensa que falta menos de um ano já mano!", "o IME é legal mas legal mesmo é não precisar mais estudar sob pressão", "VEJA A LUZ NO FIM DO TÚNEL!!", "Você é muito mais do que suas notas AHSDUIHFSUBSF"];
 var tweet = function() { 
 var msg = 'Faltam ' + diffDays(new Date(2017, 07, 31)) + ' dias pra você se formar! ' + getRandomMsg(motivationalMsgs);
+msg += "\n" + getPercentageBar(diffDays(new Date(2017, 07, 31)))
 Twitter.post('statuses/update', { status: msg }, function(err, data, response) {
   console.log(data)
 })
